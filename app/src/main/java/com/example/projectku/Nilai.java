59e80b4969ceb19f7ad353712ae82f45729381a7
ishaq1399 <ishaqhusainynur99@gmail.com>
2020-04-28 17:44:58 +0700

    rr

---
+++ app/src/main/java/com/example/projectku/Nilai.java
@@ -0,0 +1,14 @@
+package com.example.projectku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Nilai extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nilai);
+    }
+}
