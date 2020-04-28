9c615d0395eb63c9d59e4cfdf0a4e34aef3b5a24
muliyana ayu andriyana <muliyanaayu123@gmail.com>
2020-04-28 23:33:17 +0700

    nn
    nn

--- app/src/main/java/com/example/projectku/NilaiActivity.java
+++ app/src/main/java/com/example/projectku/NilaiActivity.java
@@ -7,5 +7,18 @@ import android.widget.TableRow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class NilaiActivity {
+public class NilaiActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nilai);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        TableLayout tableLayoutid = (TableLayout) this.findViewById(R.id.tabelayoutid);
+        TableRow row = (TableRow) getLayoutInflater().inflate(R.layout.layout_row,null);
+        tableLayoutid.addView(row);
+        return true;
+    }
 }
